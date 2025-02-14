e601187830bd3e0a11b3c480aed6320b32853e03
codes-harshit <programming.harshit44@gmail.com>
2025-02-14 12:50:53 +0530

    create axios api linkage

--- src/pages/Countries.jsx
+++ src/pages/Countries.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState, useTransition } from "react";
+import { countryData } from "../api/AxiosApi";
 
 const Countries = () => {
-  return <div>Countries</div>;
+  const [countries, setCountries] = useState([]);
+  const [isPending, startTransition] = useTransition();
+
+  useEffect(() => {
+    startTransition(async () => {
+      const res = await countryData();
+      setCountries(res.data);
+    });
+  }, []);
+
+  if (isPending) return <center className="heading">Loading....</center>;
+  return (
+    <>
+      <center className="heading">List of All countries </center>
+      <ul>
+        {countries.map((country) => (
+          <div>{country.name.common}</div>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default Countries;
